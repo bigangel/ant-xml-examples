41cbdeae4e1f8e661fd275283f040e6d695fdea9
Andy Li <jingxuan2046@gmail.com>
2014-12-06 00:08:08 +0800

    Added some simple build.xml examples
    Added some simple build.xml examples

---
+++ Run_Java_Project/src/com/insigma/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.insigma;
+
+import java.util.UUID;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        String id = UUID.randomUUID().toString();
+        System.out.println("UUID = " + id);
+    }
+
+}
